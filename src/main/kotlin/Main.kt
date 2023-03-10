0720033e051d6814dd9ed91913dba6f980c511ad
Anton Trifonov <royfokker.moto@gmail.com>
2023-03-10 16:53:11 +0400

    showing commission

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,10 @@ package ru.netology
 fun main() {
     val commissionRate = 0.0075
     val minimalCommission = 35.0
-    var amount = 5000.0
+    val amount = 900.0
     var commission = amount * commissionRate
     if (commission < minimalCommission) {
         commission = minimalCommission
     }
-    amount -= commission
-    println (amount)
+    println(commission)
 }
